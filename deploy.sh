c71b805eb1e0b4e1ed4527048afc28dd9402dc42
Juan Manuel Zaragoza <juan.zaragoza@teracode.com>
2018-03-24 20:00:47 -0300

    Adding custom errors on fail

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-printf "Subiendo ${GREEN}application server${NC}...\n"
+printf "Deploying ${GREEN}application server${NC}...\n"
 # set application-server user
 heroku accounts:set application-server
 # add applicationserver.git remote
@@ -18,9 +18,14 @@ heroku git:remote -a heroku-applicationserver
 git remote rename heroku heroku-applicationserver
 # push to server
 git subtree push --prefix application-server heroku-applicationserver master
-printf "${GREEN}[OK]${NC} Deploy application server terminado\n"
+if [ $? -eq 0 ]; then
+    printf "${GREEN}[OK]${NC} Deploying application server was successfully finish\n"
+else
+    printf "${GREEN}[ERROR]${NC} Deploying application server was finished with errors\n"
+fi
 
-printf "Subiendo ${GREEN}shared server${NC}...\n"
+
+printf "Deploying ${GREEN}shared server${NC}...\n"
 # set application-server user
 heroku accounts:set shared-server
 # add applicationserver.git remote
@@ -29,4 +34,8 @@ heroku git:remote -a heroku-sharedserver
 git remote rename heroku heroku-sharedserver
 # push to server
 git subtree push --prefix shared-server heroku-sharedserver master
-printf "${GREEN}[OK]${NC} Deploy application server terminado\n"
+if [ $? -eq 0 ]; then
+    printf "${GREEN}[OK]${NC} Deploy shared server terminado\n"
+else
+    printf "${GREEN}[ERROR]${NC} Deploying shared server was finished with errors\n"
+fi
