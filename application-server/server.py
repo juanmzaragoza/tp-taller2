4c2e3125b248cfbd35c4d4e66bd083c6a1bb1bd6
Juan Manuel Zaragoza <juan.zaragoza@teracode.com>
2018-03-24 18:19:39 -0300

    Changed port on application-server to start on heroku

--- application-server/server.py
+++ application-server/server.py
@@ -1,3 +1,4 @@
+import os
 from aiohttp import web
 
 async def handle(request):
@@ -11,4 +12,4 @@ app.router.add_get('/', handle)
 app.router.add_get('/{name}', handle)
 
 if __name__ == '__main__':
-    web.run_app(app, port=5858)
+    web.run_app(app, port=int(os.environ.get("PORT", 5858))
