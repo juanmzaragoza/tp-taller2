cd09b3ac36e3243d6f9f9eff8217a3b11c54fe9f
Juan Manuel Zaragoza <juan.zaragoza@teracode.com>
2018-03-25 23:10:18 -0300

    Fix python flask unit test

--- application-server/test/test_example.py
+++ application-server/test/test_example.py
@@ -2,8 +2,8 @@ import unittest
 import requests
 import json
 import sys
-import example #for local test
-#import app.example#for docker test
+import app #for local test
+#import app.app#for docker test
 
 class TestFlaskApiUsingRequests(unittest.TestCase):
     def test_hello_world(self):
@@ -13,8 +13,8 @@ class TestFlaskApiUsingRequests(unittest.TestCase):
 
 class TestFlaskApi(unittest.TestCase):
     def setUp(self):
-        self.app = example.app.test_client()
-	#self.app = app.example.app.test_client()#for docker test
+        self.app = app.app.test_client()
+        #self.app = app.app.app.test_client()#for docker test
 
     def test_hello_world(self):
         response = self.app.get('/')
