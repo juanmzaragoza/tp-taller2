4cf94b8001c9b8456e6f6c9e20ebc38b5a3f926a
mevk <masrianv@gmail.com>
2018-03-30 15:42:24 -0300

    MongoDB - Flask - Travis Integration

--- application-server/app.py
+++ application-server/app.py
@@ -1,14 +1,25 @@
 import flask
 import flask_restful
+from flask_pymongo import PyMongo
 
 app = flask.Flask(__name__)
-api = flask_restful.Api(app)
+with app.app_context():
 
-class HelloWorld(flask_restful.Resource):
-    def get(self):
-        return {'hello': 'world'}
+	app.config['MONGO_DBNAME'] = 'test'
+	app.config['MONGO_URI'] = 'mongodb://localhost:27017/test'
+	#app.config['MONGO_URI'] = 'mongodb://mongo:27017/test';#for docker test
 
-api.add_resource(HelloWorld, '/')
+	api = flask_restful.Api(app)
+	mongo = PyMongo(app)
+	doc = mongo.db.test.insert({'user':'root'})
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5858,debug=True)
+	class HelloWorld(flask_restful.Resource):
+		def get(self):
+			root = mongo.db.test.find_one_or_404({'user': 'root'})
+			root_name = root.get('name')
+			return {'hello': root_name}
+
+	api.add_resource(HelloWorld, '/')
+
+	if __name__ == "__main__":
+    		app.run(host='0.0.0.0', port=5858,debug=True)
--- application-server/test/test_example.py
+++ application-server/test/test_example.py
@@ -8,18 +8,17 @@ import app #for local test
 class TestFlaskApiUsingRequests(unittest.TestCase):
     def test_hello_world(self):
         response = requests.get('http://0.0.0.0:5858')
-        self.assertEqual(response.json(), {'hello': 'world'})
+        self.assertEqual(response.json(), {'hello': 'Jose'})
 
 
 class TestFlaskApi(unittest.TestCase):
     def setUp(self):
-        self.app = app.app.test_client()
+        self.app = app.app.test_client()#for local test
         #self.app = app.app.app.test_client()#for docker test
 
     def test_hello_world(self):
         response = self.app.get('/')
-        self.assertEqual(json.loads(response.get_data().decode(sys.getdefaultencoding())), {'hello': 'world'})
-
+        self.assertEqual(json.loads(response.get_data().decode(sys.getdefaultencoding())), {'hello': 'Jose'})
 
 if __name__ == "__main__":
     unittest.main()
