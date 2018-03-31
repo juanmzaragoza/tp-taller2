0856f202fe6a3caee55db6b9c375d613edfa154a
kaoru <kaoruheanna@gmail.com>
2018-03-31 18:43:43 -0300

    merging

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,11 +1,20 @@
-import { NgModule }      from '@angular/core';
+import { MaterializeModule } from 'angular2-materialize';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppComponent }  from './app.component';
+
+import { AppComponent } from './app.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    MaterializeModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
