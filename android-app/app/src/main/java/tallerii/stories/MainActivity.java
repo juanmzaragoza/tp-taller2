e1976fdf17ffe72c2bb4a9dd51d9aa30cbdb2327
Juan Manuel Zaragoza <juan.zaragoza@teracode.com>
2018-03-31 18:07:14 -0300

    Add bottom nav menu with basic logic

--- android-app/app/src/main/java/tallerii/stories/MainActivity.java
+++ android-app/app/src/main/java/tallerii/stories/MainActivity.java
@@ -1,8 +1,11 @@
 package tallerii.stories;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,5 +22,32 @@ public class MainActivity extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.usernameView);
         textView.setText(message);
+
+        // get bottom navigation menu
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        // on click one, change content
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    return switchContent(item);
+                }
+            });
     }
+
+    private boolean switchContent(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                // change content
+            case R.id.action_account:
+                // change content
+            case R.id.action_new:
+                // change content
+        }
+        return true;
+    }
+
+
 }
