08b4107e07a73d0be816ab39bfeeae9226e08f2f
Edward Erik <erikschmoll@gmail.com>
2018-04-08 18:18:02 -0300

    wrong user or password, add token into response /user

--- shared-server/app/controllers/user.controller.js
+++ shared-server/app/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const config      = require('../../config/default')
 const messages    = require('../../config/messages')
 const StorageServ = require('../services/storage.service')
+const AuthServ    = require('../services/auth.service')
 const User        = require('../models/user')
 const ResServ     = require('../services/response.service')
 const ResEnum     = require('../common/response.enum')
@@ -12,10 +13,11 @@ class UserController {
             StorageServ.save('user',usr, (id)=>{
                 if(id){
                     usr.id = id;
+                    usr["token"] = AuthServ.token(usr.username)
                     ResServ.ok(ResEnum.Value, "user", usr, res, next);
                 }
                 else{
-                    ResServ.error(500, messages.common.error, res, next);
+                    ResServ.error(500, 2, messages.common.error, res, next);
                 }
             });
         };
@@ -26,7 +28,7 @@ class UserController {
                     ResServ.ok(ResEnum.Value, "user", usr, res, next);
                 }
                 else{
-                    ResServ.error(500, messages.common.error, res, next);
+                    ResServ.error(401, 1, messages.user.notExist, res, next);
                 }
             });
         };
