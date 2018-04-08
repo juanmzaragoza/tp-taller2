8f5c932354e4e520070a6fc4be8ce9eeb1ad7582
Nicolas Dominguez <nicolas.dominguez@avature.net>
2018-04-08 20:38:08 -0300

    Unit testing of login logic with mocking

--- android-app/app/src/main/java/tallerii/stories/network/ConstantsApplicationApiRest.java
+++ android-app/app/src/main/java/tallerii/stories/network/ConstantsApplicationApiRest.java
@@ -1,10 +1,10 @@
 package tallerii.stories.network;
 
 public final class ConstantsApplicationApiRest {
-    public static final String ROOT_URL = "https://httpbin.org/";
+    public static String ROOT_URL = "https://httpbin.org/";
     public static final String REST_API = "";
 
-    public static final String KEY_GET_USER_BY_ID = "get?username=juanzaragoza@live.com.ar&password=gk3nxmbkif&facebookAuthToken=gaksrgnnbxc";
+    public static final String KEY_GET_USER_BY_ID = "get";
 
-    public static final String URL_GET_USER_BY_ID = ROOT_URL + REST_API + KEY_GET_USER_BY_ID;
+    public static final String URL_GET_USER_BY_ID = REST_API + KEY_GET_USER_BY_ID;
 }
---
+++ android-app/app/src/test/java/tallerii/stories/LoginControllerTest.java
@@ -0,0 +1,66 @@
+package tallerii.stories;
+
+import com.squareup.okhttp.mockwebserver.MockResponse;
+import com.squareup.okhttp.mockwebserver.MockWebServer;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.io.IOException;
+
+import tallerii.stories.network.ConstantsApplicationApiRest;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+
+@RunWith(JUnit4.class)
+public class LoginControllerTest {
+
+    private MockWebServer server;
+
+    @Before
+    public void setUp() throws IOException {
+        server = new MockWebServer();
+        server.start();
+        ConstantsApplicationApiRest.ROOT_URL = server.url("/").toString();
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        server.shutdown();
+    }
+
+    @Test
+    public void testCheckFBUserExistsWithExistingUserShowsError() {
+        LoginActivity mockActivity = mock(LoginActivity.class);
+        LoginController controller = new LoginController(mockActivity);
+
+        controller.checkFBUserExists(1, "");
+
+        server.enqueue(new MockResponse().setBody("{\"username\":\"user\"}"));
+
+        int requests = server.getRequestCount();
+        verify(mockActivity, timeout(2000).times(1)).showMessage(anyString());
+    }
+
+    @Test
+    public void testCheckFBUserCallsRegistrationActivity() {
+        LoginActivity mockActivity = mock(LoginActivity.class);
+        LoginController controller = new LoginController(mockActivity);
+
+        controller.checkFBUserExists(1, "");
+
+        server.enqueue(new MockResponse());
+
+        int requests = server.getRequestCount();
+        verify(mockActivity, timeout(2000).times(1)).startRegistrationActivity(anyString());
+    }
+}
