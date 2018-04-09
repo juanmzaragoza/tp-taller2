59b0f6fda6def8c3606b2d316126ea12f1f870e9
Edward Erik <erikschmoll@gmail.com>
2018-04-09 15:04:23 -0300

    view User

--- web/src/app/components/login/login.component.ts
+++ web/src/app/components/login/login.component.ts
@@ -4,6 +4,7 @@ import { LoginService } from '../../services/login/login.service'
 import { User }         from '../../models/user'
 import { Observable }   from 'rxjs/Observable';
 import { Location }     from '@angular/common';
+import { SharedService } from '../../services/common/shared.service'
 
 @Component({
     templateUrl: './login.component.html'
@@ -12,20 +13,23 @@ export class LoginComponent {
     private user:User = new User('','');
     constructor(public loginServ :LoginService, 
                 public RouterServ : Router,
-                public Location: Location){}
+                public Location: Location,
+                public SharedServ: SharedService){}
 
     login(user: User) {
         this.loginServ.token(user).subscribe((val) => {
           if(val){
-              this.RouterServ.navigate(['/home']);
+                this.SharedServ.startAccount.emit(user.username);
+                this.RouterServ.navigate(['/home']);
           }
           else{
-              console.info('credencial incorrecta');
+                console.info('credencial incorrecta');
           }
         });
       }
       back(){
-        this.Location.back();
+        //this.Location.back();
+        this.RouterServ.navigate(['/home']);
       }
 }
 
