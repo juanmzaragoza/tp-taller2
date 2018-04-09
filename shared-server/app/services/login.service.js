a859e11e8f3a4925aebf5ccf6db14020ae48a8a5
Edward Erik <erikschmoll@gmail.com>
2018-04-09 10:04:11 -0300

    User Admin Web

--- shared-server/app/services/login.service.js
+++ shared-server/app/services/login.service.js
@@ -1,30 +1,45 @@
 "use strict";
-const AuthService   = require('./auth.service')
-const StorageServ   = require('./storage.service')
-const bcrypt        = require('bcrypt');
+const AuthService       = require('./auth.service')
+const StorageServ       = require('./storage.service')
+const LocalStorageServ  = require('./local.storage.service')
+const bcrypt            = require('bcrypt');
 
 
 class LoginService {
     constructor() {
         this.auth = ( username, password, next) => {
+            var me = this;
             StorageServ.load('user', "username", username, (usr)=>{
                 if(usr){
-                    bcrypt.compare(password, usr.password, function(err, res) {
-                        var result = undefined;
-                        if(res == true){
-                            result =  { 
-                                token: AuthService.token(usr),
-                                expiresAt: 3600
-                            };
-                        }
-                        next(result);
-                    });
+                    //users of the application-server
+                    me.generateToken(usr, password, next);
                 }
                 else{
-                    next(undefined);
+                    //users of the shared-server
+                    LocalStorageServ.load(username, (usrAdmin) =>{
+                        if(usrAdmin){
+                            me.generateToken(usrAdmin, password, next);
+                        }
+                        else{
+                            //unknown
+                            next(undefined);
+                        }
+                    })
                 }
             });
         };
+        this.generateToken = (user, password, next)=>{
+            bcrypt.compare(password, user.password, function(err, res) {
+                var result = undefined;
+                if(res == true){
+                    result =  { 
+                        token: AuthService.token(user),
+                        expiresAt: 3600
+                    };
+                }
+                next(result);
+            });
+        }
     }
 }
 module.exports = new LoginService();
