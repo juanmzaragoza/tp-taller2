01051995e0f582b8681a309f4efc4ccd542e183c
mevk <masrianv@gmail.com>
2018-04-15 00:11:44 -0300

    using ResponseBuilder

--- application-server/controllers/ping_controller.py
+++ application-server/controllers/ping_controller.py
@@ -1,11 +1,15 @@
 import flask_restful
+from controllers.response_builder import ResponseBuilder
+
+import json
 
 class PingController(flask_restful.Resource):
-
-	def get(self):	
-		return self.__ping_response()
-
-	def __ping_response(self):
+	
+	def get(self):
+		response = self.__ping_successful_response()
+		return ResponseBuilder.build_response(response, 200)
+		
+	def __ping_successful_response(self):
 		return {
 			"status": "active"
-		}, 200
+		}
