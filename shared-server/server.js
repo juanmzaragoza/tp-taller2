407d0a46777305efb04f1a8f178fbd5f8f804085
kaoru <kaoruheanna@gmail.com>
2018-04-15 15:02:35 -0300

    integration sequlize

--- shared-server/server.js
+++ shared-server/server.js
@@ -36,9 +36,44 @@ app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
 
+// Database
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(
+	config.get('database.database'),
+	config.get('database.username'),
+	config.get('database.password'),
+	{
+		host: config.get('database.host'),
+		port : config.get('database.port'),
+		dialect: config.get('database.dialect'),
+		operatorsAliases: false,
+		pool: {
+			max: 5,
+			min: 0,
+			acquire: 30000,
+			idle: 10000
+		},
+	});
+
+sequelize
+	.authenticate()
+	.then(() => {
+		console.log('Connection has been established successfully.');
+
+
+		sequelize.query("SELECT * FROM prueba", { type: sequelize.QueryTypes.SELECT})
+		.then(prueba => {
+			console.log("prueba:", prueba);
+		})
+	})
+	.catch(err => {
+		console.error('Unable to connect to the database:', err);
+	});
+
+
 //start
 var server = app.listen(PORT, HOST);
-console.log('server atarted and listening on ' + PORT + ':' + HOST);
+console.log('server started and listening on ' + PORT + ':' + HOST);
 
 //to start and stop in tests
 module.exports = server;
