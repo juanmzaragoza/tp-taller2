eb31c7d4905be624dcf83c867837208b699d5e0c
mevk <masrianv@gmail.com>
2018-04-15 23:02:03 -0300

    heroku mongodb added

--- application-server/controllers/db_controller.py
+++ application-server/controllers/db_controller.py
@@ -1,13 +1,13 @@
 import flask
 import flask_restful
 import pymongo
-from constants import MONGODB_LOCAL_URI, MONGODB_DOCKER_URI
+from constants import MONGODB_LOCAL_URI, MONGODB_DOCKER_URI, MONGODB_HEROKU_URI
 
 class MongoController(flask_restful.Resource):
 	@staticmethod
 	def get_mongodb_instance(user, passwd):
 		try:
-			conn=pymongo.MongoClient(MONGODB_LOCAL_URI)
+			conn=pymongo.MongoClient(MONGODB_HEROKU_URI)
 			conn.appserverdb.authenticate(user, passwd, mechanism = 'SCRAM-SHA-1')
 			db = conn.appserverdb
 			return db
