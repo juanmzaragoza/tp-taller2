20140dcfa80743c55ca06f016d8a6c230449f344
Edward Erik <erikschmoll@gmail.com>
2018-04-18 22:14:33 -0300

    separo el servicio del storage

--- shared-server/app/controllers/server.controller.js
+++ shared-server/app/controllers/server.controller.js
@@ -1,16 +1,24 @@
-const config      = require('../../config/default')
-const messages    = require('../../config/messages')
-const StorageServ = require('../services/storage.service')
-const AuthServ    = require('../services/auth.service')
-const Server      = require('../models/server')
-const ResServ     = require('../services/response.service')
-const ResEnum     = require('../common/response.enum')
+const config        = require('../../config/default')
+const messages      = require('../../config/messages')
+const AuthServ      = require('../services/auth.service')
+const Server        = require('../models/server')
+const ResServ       = require('../services/response.service')
+const ResEnum       = require('../common/response.enum')
+const ServerService = require('../services/server.service')
 
 class ServerController {
     constructor() {
         this.post = (req, res, next) => {
             var srv = new Server(req.body);
-            StorageServ.saveServer('server',srv, (id)=>{
+            ServerService.add(srv, (err, server)=>{
+                if(err){
+                    ResServ.error(404, 2, messages.common.error, res, next);
+                }
+                else{
+                    ResServ.ok(ResEnum.Value, "server", server, res, next);
+                }
+            })
+            /*StorageServ.saveServer('server',srv, (id)=>{
                 if(id){
                     srv.id = id;
                     ResServ.ok(ResEnum.Value, "server", srv, res, next);
@@ -18,29 +26,37 @@ class ServerController {
                 else{
                     ResServ.error(500, 2, messages.common.error, res, next);
                 }
-            });
+            });*/
         };
         this.get = (req, res, next) => {
-            StorageServ.loadAll('server', (arr)=>{
+            ServerService.get((err, servers)=>{
+                if(err){
+                    ResServ.error(404, 2, messages.common.error, res, next);
+                }
+                else{
+                    ResServ.ok(ResEnum.Values, "servers", servers, res, next);
+                }
+            })
+            /*StorageServ.loadAll('server', (arr)=>{
                 if(arr){
                     ResServ.ok(ResEnum.Values, "servers", arr, res, next);
                 }
                 else{
                     ResServ.error(500, 2, messages.common.error, res, next);
                 }
-            });
+            });*/
         };
         this.delete = (req, res, next) => {
-            console.info("z")
             var id = req.params.id;
-            StorageServ.delete('server', id, (ok)=>{
-                if(ok){
-                    ResServ.ok(ResEnum.OnlyValue, undefined, "Baja correcta", res, next);
+            ServerService.delete(id, (err, message)=>{
+                console.info(err)
+                if(err){
+                    ResServ.error(404, 2, messages.common.error, res, next);
                 }
                 else{
-                    ResServ.error(500, 2, messages.common.error, res, next);
+                    ResServ.ok(ResEnum.OnlyValue, undefined, message, res, next);
                 }
-            });
+            })
         };
     }
 }
