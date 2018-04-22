86d8b8a5a273afecc8165bfa5f69437c0fcd174b
kaoru <kaoruheanna@gmail.com>
2018-04-22 15:11:06 -0300

    login service test

---
+++ shared-server/test/loginServiceTest.js
@@ -0,0 +1,75 @@
+var expect = require('chai').expect;
+var assert = require('chai').assert;
+var request = require('request');
+var loginServ   = require("../app/services/login.service");
+var SequelizeMock = require('sequelize-mock');
+
+var models;
+
+before(function() {
+	var dbMock = new SequelizeMock();
+	var UserMock = dbMock.define('user', {
+	}, {
+	    instanceMethods: {
+	    },
+	});	
+
+	UserMock.$queryInterface.$useHandler(function(query, queryOptions, done) {
+	    if (query === 'findOne') {
+	        if (queryOptions[0].where.username == 'admin') {
+	            // Result found, return it
+	            return UserMock.build({ 
+	            	id: 1,
+				    username: 'admin',
+				    password: '$2b$10$dtJNzWyXhAQa6PdglSjug.XMlNLuf.58cORvB5UAEQmY8vf38DVHG', //1234
+				    token: null,
+				    token_expiration: null
+			    });
+	        } else {
+	            // No results
+	            return null;
+	        }
+	    }
+	});
+
+	models = {
+		user: UserMock
+	};
+});
+
+it ('Invalid user should throw unauthorized', function(done) {
+	loginServ.auth('test','1234', models)
+	.then((result) => {
+		assert(false);
+        done();
+    })
+    .catch((reason) => {
+        assert.equal(reason, 'unauthorized');
+        done();
+    });
+});
+
+it ('Invalid password should throw unauthorized', function(done) {
+	loginServ.auth('admin','qweqwe', models)
+	.then((result) => {
+		assert(false);
+        done();
+    })
+    .catch((reason) => {
+        assert.equal(reason, 'unauthorized');
+        done();
+    });
+});
+
+it ('Valid credentials should success', function(done) {
+	loginServ.auth('admin','1234', models)
+	.then((result) => {
+		assert.isTrue(result.hasOwnProperty('token'));
+		assert.isTrue(result.hasOwnProperty('expiresAt'));
+		done();
+    })
+    .catch((reason) => {
+        assert(false);
+        done();
+    });
+});
