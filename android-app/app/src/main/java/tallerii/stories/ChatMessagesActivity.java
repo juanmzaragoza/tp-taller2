9a0969d5e7050250ce6d2087e05448a5d6529d56
Nicolas Dominguez <nicolas.dominguez@avature.net>
2018-04-30 21:38:26 -0300

    Initial messaging activity
    can send messages!! pull is not working though

---
+++ android-app/app/src/main/java/tallerii/stories/ChatMessagesActivity.java
@@ -0,0 +1,149 @@
+package tallerii.stories;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import tallerii.stories.helpers.MessagesAdapter;
+import tallerii.stories.network.apimodels.ChatMessage;
+
+public class ChatMessagesActivity extends AppCompatActivity {
+
+    private RecyclerView mChatsRecyclerView;
+    private EditText mMessageEditText;
+    private DatabaseReference mMessagesDBRef;
+    private List<ChatMessage> mMessagesList = new ArrayList<>();
+
+    private String receiverId;
+    private String receiverName;
+    private String currentUserId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_messages);
+
+        //initialize the views
+        mChatsRecyclerView = findViewById(R.id.messagesRecyclerView);
+        mMessageEditText = findViewById(R.id.messageEditText);
+        mChatsRecyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mLayoutManager.setStackFromEnd(true);
+        mChatsRecyclerView.setLayoutManager(mLayoutManager);
+
+        //init Firebase
+        mMessagesDBRef = FirebaseDatabase.getInstance().getReference().child("Messages");
+
+        //get receiverId from intent
+        receiverId = "2";
+        currentUserId = "1";
+    }
+
+    public void sendMessage(View v) {
+        String message = mMessageEditText.getText().toString();
+        //TODO use our user id
+        //Todo disable button if empty
+        if(message.isEmpty()){
+            Toast.makeText(ChatMessagesActivity.this, "You must enter a message", Toast.LENGTH_SHORT).show();
+        }else {
+            //message is entered, send
+            sendMessageToFirebase(message, currentUserId, receiverId);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        /*Query and populate chat messages*/
+        queryMessages();
+        /*sets title bar with recepient name*/
+        queryRecipientName(receiverId);
+    }
+
+
+
+    private void sendMessageToFirebase(String message, String senderId, String receiverId){
+        mMessagesList.clear();
+
+        ChatMessage newMsg = new ChatMessage(message, senderId, receiverId);
+        mMessagesDBRef.push().setValue(newMsg).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(!task.isSuccessful()){
+                    //error
+                    Toast.makeText(ChatMessagesActivity.this, "Error " + task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(ChatMessagesActivity.this, "Message sent successfully!", Toast.LENGTH_SHORT).show();
+                    mMessageEditText.setText(null);
+                    hideSoftKeyboard();
+                }
+            }
+        });
+
+
+    }
+
+    public void hideSoftKeyboard() {
+        if(getCurrentFocus()!=null) {
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            assert inputMethodManager != null;
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+    }
+
+    private void queryMessages(){
+        mMessagesDBRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                mMessagesList.clear();
+
+                for(DataSnapshot snap: dataSnapshot.getChildren()){
+                    ChatMessage chatMessage = snap.getValue(ChatMessage.class);
+                    //TODO get them with a select like query?
+                    //TODO this listens to all changes???????? cant we just be notified of necessary? investigate
+                    assert chatMessage != null;
+                    if(chatMessage.getSenderId().equals(currentUserId) && chatMessage.getReceiverId().equals(receiverId) || chatMessage.getSenderId().equals(receiverId) && chatMessage.getReceiverId().equals(currentUserId)){
+                        mMessagesList.add(chatMessage);
+                    }
+
+                }
+                populateMessagesRecyclerView();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                //TODO manage cancels?? is this possible?
+            }
+        });
+    }
+
+    private void populateMessagesRecyclerView(){
+        MessagesAdapter adapter = new MessagesAdapter(mMessagesList, this);
+        mChatsRecyclerView.setAdapter(adapter);
+
+    }
+
+    private void queryRecipientName(final String receiverId){
+        //TODO get and set receiver name
+        receiverName = "Guido puto";
+    }
+}
---
+++ android-app/app/src/main/java/tallerii/stories/helpers/MessagesAdapter.java
@@ -0,0 +1,88 @@
+package tallerii.stories.helpers;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.List;
+
+import tallerii.stories.R;
+import tallerii.stories.network.apimodels.ChatMessage;
+
+
+public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.ViewHolder> {
+    private static final int ITEM_TYPE_SENT = 0;
+    private static final int ITEM_TYPE_RECEIVED = 1;
+
+    private List<ChatMessage> messagesList;
+    private Context mContext;
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView messageTextView;
+        public View layout;
+
+        public ViewHolder(View v) {
+            super(v);
+            layout = v;
+            messageTextView = v.findViewById(R.id.chatMsgTextView);
+        }
+    }
+
+    public void add(int position, ChatMessage message) {
+        messagesList.add(position, message);
+        notifyItemInserted(position);
+    }
+
+    public void remove(int position) {
+        messagesList.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public MessagesAdapter(List<ChatMessage> dataset, Context context) {
+        messagesList = dataset;
+        mContext = context;
+
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        //TODO use our user id
+        if (messagesList.get(position).getSenderId().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
+            return ITEM_TYPE_SENT;
+        } else {
+            return ITEM_TYPE_RECEIVED;
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = null;
+        if (viewType == ITEM_TYPE_SENT) {
+            v = LayoutInflater.from(mContext).inflate(R.layout.sent_message_row, null);
+        } else if (viewType == ITEM_TYPE_RECEIVED) {
+            v = LayoutInflater.from(mContext).inflate(R.layout.received_message_row, null);
+        }
+        return new ViewHolder(v); // view holder for header items
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        ChatMessage msg = messagesList.get(position);
+        holder.messageTextView.setText(msg.getMessage());
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return messagesList.size();
+    }
+}
