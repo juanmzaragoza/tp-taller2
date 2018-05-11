6f5925b34c4021c5d40a83241b7199848fb3a885
Edward Erik <erikschmoll@gmail.com>
2018-05-11 09:10:34 -0300

    agrego mock

--- web/src/app/services/status/status.service.ts
+++ web/src/app/services/status/status.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable }   from '@angular/core';
+import { Observable }   from 'rxjs/Observable';
 
+import 'rxjs/add/observable/of';
 
 @Injectable()
 export class StatusService {
+    public request: Array<any>;
     constructor(){}
     
+    getRequests = ()=>{
+        var x = Math.floor((Math.random() * 10) + 1)
+        this.request = [
+            {frequency: 0, count: 0},
+            {frequency: 1, count: 3},
+            {frequency: 2, count: x}
+        ]
+        Observable.of(this.request);
+    }
 } 
