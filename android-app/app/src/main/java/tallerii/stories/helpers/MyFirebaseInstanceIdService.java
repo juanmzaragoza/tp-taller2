31bac1bb185aa33125976c4a40beca18984bdd0c
Nicolas Dominguez <nicolas.dominguez@avature.net>
2018-05-27 22:09:28 -0300

    Add firbase services

---
+++ android-app/app/src/main/java/tallerii/stories/helpers/MyFirebaseInstanceIdService.java
@@ -0,0 +1,21 @@
+package tallerii.stories.helpers;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        if (refreshedToken != null) {
+            Log.d(TAG, "Refreshed token: " + refreshedToken);
+            // TODO actually use tokens to have security
+        }
+    }
+}
