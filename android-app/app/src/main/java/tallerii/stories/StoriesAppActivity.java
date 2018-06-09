6a467ae1946919741224fd478112d7e912e4ddcc
Juan Manuel Zaragoza <juan.zaragoza@teracode.com>
2018-06-09 15:50:49 -0300

    Fixing layout overlapping problem on stories list. Avoided screen rotation

--- android-app/app/src/main/java/tallerii/stories/StoriesAppActivity.java
+++ android-app/app/src/main/java/tallerii/stories/StoriesAppActivity.java
@@ -2,6 +2,7 @@ package tallerii.stories;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -30,6 +31,7 @@ public abstract class StoriesAppActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         checkGooglePlayServices();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
     private void checkGooglePlayServices() {
