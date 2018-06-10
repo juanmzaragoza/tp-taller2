ea19e2c8cc7b643ee476457f4c272919eec7b782
kaoru <kaoruheanna@gmail.com>
2018-06-10 18:35:26 -0300

    update file tests

--- shared-server/app/models/server.js
+++ shared-server/app/models/server.js
@@ -1,21 +1,21 @@
 
-class Server{
-    constructor(data){
-        this.id;
-        this._rev = data._rev;
-        this.createdBy = data.createdBy;
-        this.createdTime = data.createdTime;
-        this.name = data.name;
-        this.lastConnection = data.lastConnection;
-        this.host = data.host;
-        this.port = data.port;
-        this.ping = data.ping;
-        this.token;
-        this.stats = data.stats
-    }
-}
+// class Server{
+//     constructor(data){
+//         this.id;
+//         this._rev = data._rev;
+//         this.createdBy = data.createdBy;
+//         this.createdTime = data.createdTime;
+//         this.name = data.name;
+//         this.lastConnection = data.lastConnection;
+//         this.host = data.host;
+//         this.port = data.port;
+//         this.ping = data.ping;
+//         this.token;
+//         this.stats = data.stats
+//     }
+// }
 
 
-module.exports = Server;
+// module.exports = Server;
 
 
--- shared-server/test/fileServiceTest.js
+++ shared-server/test/fileServiceTest.js
@@ -69,6 +69,110 @@ describe('File Service Tests', function(){
 		});
 	});
 
+	it ('Update file with missing attrs should throw invalid-attrs', function(done) {
+		var attrs = {
+			'rev': '123123123'
+		};
+		fileService.update(14, attrs, models)
+		.then((result) => {
+			assert(false);
+			done();
+		})
+		.catch((reason) => {
+			assert.equal(reason, 'invalid-attrs');
+			done();
+		});
+	});
+
+	it ('Update inexistent file should throw not-found', function(done) {
+		models.file.$queryInterface.$useHandler(function(query, queryOptions, done) {
+		    if (query === 'findById') {
+		    	if (queryOptions[0] === 14) {
+		            return null
+		        }
+		    }
+		});
+		var attrs = {
+			'rev': '123123123',
+			'filename': 'lalal'
+		};
+		fileService.update(14, attrs, models)
+		.then((result) => {
+			assert(false);
+			done();
+		})
+		.catch((reason) => {
+			assert.equal(reason, 'not-found');
+			done();
+		});
+	});
+
+	it ('Update file with invalid rev should throw conflict', function(done) {
+		models.file.$queryInterface.$useHandler(function(query, queryOptions, done) {
+		    if (query === 'findById') {
+		    	if (queryOptions[0] === 12) {
+		    		return models.file.build({ 
+		            	"id": 12,
+		            	"rev": "456",
+						"createdTime": "2018-05-26T17:19:51.342Z",
+						"updatedTime": "2018-05-26T17:19:51.342Z",
+						"filename": "dummy"
+				    });
+		        } else {
+		        	return null;
+		        }
+		    }
+		});
+		var attrs = {
+			'rev': '123',
+			'filename': 'lalal'
+		};
+		fileService.update(12, attrs, models)
+		.then((result) => {
+			assert(false);
+			done();
+		})
+		.catch((reason) => {
+			assert.equal(reason, 'conflict');
+			done();
+		});
+	});
+
+	it ('Update file should success', function(done) {
+		models.file.$queryInterface.$useHandler(function(query, queryOptions, done) {
+			if (query === 'findById') {
+		    	if (queryOptions[0] == 5) {
+		    		return models.file.build({ 
+		            	"id": 5,
+		            	"rev": "456",
+						"createdTime": "2018-05-26T17:19:51.342Z",
+						"updatedTime": "2018-05-26T17:19:51.342Z",
+						"filename": "dummy",
+						"size": 0,
+						"resource": null
+				    });
+		        } else {
+		        	return null;
+		        }
+		    }
+		});
+		var attrs = {
+			filename: "new name",
+			rev: "456"
+		};
+		fileService.update(5, attrs, models)
+		.then((response) => {
+			assert.notEqual(response,null);
+			assert.equal(response.id,5);
+			assert.equal(response.filename,'new name');
+			assert.notEqual(response._rev,"456");
+			done();
+		})
+		.catch((reason) => {
+			assert(false, "Update failed: "+reason);
+			done();
+		});
+	});
 
 
 });
