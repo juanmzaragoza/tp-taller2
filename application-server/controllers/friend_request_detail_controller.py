b79ecd6576f22224108ecc729d6a679915f57469
kaoru <kaoruheanna@gmail.com>
2018-06-24 17:23:08 -0300

    validations on friend_request_detail_controller

--- application-server/controllers/friend_request_detail_controller.py
+++ application-server/controllers/friend_request_detail_controller.py
@@ -8,27 +8,39 @@ from controllers.friend_controller import FriendController
 from api_client.db_connection_error import DBConnectionError
 from errors_exceptions.no_data_found_exception import NoDataFoundException
 from errors_exceptions.no_friend_request_found_exception import NoFriendRequestFoundException
-from auth_service import login_required
+from errors_exceptions.user_mismatch_exception import UserMismatchException
+from auth_service import login_required, get_user_id
 
 class FriendRequestDetailController(flask_restful.Resource):
 	
+	@login_required
 	def get(self, request_id):
 		try:
 			 self._validate_request_id(request_id)
 			 friend_request = FriendRequestModel.get_friend_request(request_id)
+			 self._validate_request_participant(friend_request)
 			 return self._get_friends_requests_response(friend_request)
 		except NoFriendRequestFoundException as e:
 			return ErrorHandler.create_error_response(str(e), 404)
+		except UserMismatchException as e:
+			return ErrorHandler.create_error_response(str(e), 409)
 		except DBConnectionError as e:
 			return ErrorHandler.create_error_response(str(e), 500)
 			
+	@login_required
 	def delete(self, request_id):
 		try:
-			 self._validate_request_id(request_id)
-			 friend_request = FriendRequestModel.remove_friend_request(request_id)
-			 return self._get_friends_requests_response(friend_request)
+			self._validate_request_id(request_id)
+
+			friend_request = FriendRequestModel.get_friend_request(request_id)
+			self._validate_request_participant(friend_request)
+
+			friend_request = FriendRequestModel.remove_friend_request(request_id)
+			return self._get_friends_requests_response(friend_request)
 		except NoFriendRequestFoundException as e:
 			return ErrorHandler.create_error_response(str(e), 404)
+		except UserMismatchException as e:
+			return ErrorHandler.create_error_response(str(e), 409)
 		except DBConnectionError as e:
 			return ErrorHandler.create_error_response(str(e), 500)
 			
@@ -37,3 +49,10 @@ class FriendRequestDetailController(flask_restful.Resource):
 		 		 
 	def _get_friends_requests_response(self, friends_requests):
 		return friends_requests
+
+	def _validate_request_participant(self, friend_request):
+		sender_user_id = int(friend_request.get('user_id_sender'))
+		receiver_user_id = int(friend_request.get('user_id_rcv'))
+		user_id = int(get_user_id())
+		if (user_id not in [sender_user_id, receiver_user_id]):
+			raise UserMismatchException()
