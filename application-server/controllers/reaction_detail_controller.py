b458f5b8ba5c161edffc6de92a38f54f9eae3c87
kaoru <kaoruheanna@gmail.com>
2018-06-24 17:52:22 -0300

    validations on delete reaction

--- application-server/controllers/reaction_detail_controller.py
+++ application-server/controllers/reaction_detail_controller.py
@@ -7,20 +7,30 @@ from controllers.response_builder import ResponseBuilder
 from api_client.db_connection_error import DBConnectionError
 from errors_exceptions.data_version_exception import DataVersionException
 from errors_exceptions.no_reaction_found_exception import NoReactionFoundException
-from auth_service import login_required
+from errors_exceptions.user_mismatch_exception import UserMismatchException
+from auth_service import login_required, validate_sender
 from models.user_activity import UserActivityModel
 
 class ReactionDetailController(flask_restful.Resource):
 	
+	@login_required
 	def delete(self, reaction_id):
 		try:
-			 reaction = ReactionModel.remove_reaction(reaction_id)
-			 UserActivityModel.log_reaction_activity(reaction["user_id"], reaction["storie_id"], reaction["reaction"], "DELETE")
-			 return self._get_reactions_response(reaction)
+			self._validate_author(reaction_id)
+			reaction = ReactionModel.remove_reaction(reaction_id)
+			UserActivityModel.log_reaction_activity(reaction["user_id"], reaction["storie_id"], reaction["reaction"], "DELETE")
+			return self._get_reactions_response(reaction)
 		except NoReactionFoundException as e:
 			return ErrorHandler.create_error_response(str(e), 404)
+		except UserMismatchException as e:
+			return ErrorHandler.create_error_response(str(e), 409)
 		except DBConnectionError as e:
 			return ErrorHandler.create_error_response(str(e), 500)
 					 		 
 	def _get_reactions_response(self, reactions):
 		return reactions
+
+	def _validate_author(self, reaction_id):
+		reaction = ReactionModel.get_reaction_with_id(reaction_id)
+		author_id = reaction.get('user_id')
+		validate_sender(author_id)
