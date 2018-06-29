07c2c44bd2f15c6550c73c89444353791706c054
Nicolas Dominguez <nicolas.dominguez@avature.net>
2018-06-29 12:39:14 -0300

    Add friend reqeust test for creation

--- application-server/mocks/friend_requests_successful_mock.py
+++ application-server/mocks/friend_requests_successful_mock.py
@@ -1,17 +1,27 @@
 friends_requests_raw_mock = [{
 	"_id": "5ae66a31d4ef925dac59a94a",
-	"date": "01/05/2018", 
-	"last_name": "Fernandez", 
+	"date": "01/05/2018",
+	"last_name": "Fernandez",
 	"name": "Carlos",
-	"message": "Hello", 
+	"message": "Hello",
 	"user_id": "5ae66a31d4ef925dac59a94e"
 }]
 
 friends_requests_successful_mock = [{
 	"_id": "5ae66a31d4ef925dac59a94a",
-	"date": "01/05/2018", 
-	"last_name": "Fernandez", 
-	"name": "Carlos", 
-	"message": "Hello", 
+	"date": "01/05/2018",
+	"last_name": "Fernandez",
+	"name": "Carlos",
+	"message": "Hello",
 	"user_id": "5ae66a31d4ef925dac59a94e"
 }]
+
+friends_requests_creation_successful_mock = {
+	'_id': '',
+	'user_id_sender': 1,
+	'user_id_rcv': 5,
+	'message': 'hello',
+	'picture': 'picId',
+	'date': ''
+}
+
--- application-server/test/unit_tests/friend_request_test.py
+++ application-server/test/unit_tests/friend_request_test.py
@@ -1,13 +1,15 @@
 import unittest
 import unittest.mock as mock
-from models.friend_request import FriendRequestModel
-from controllers.be_friend_detail_controller import BeFriendDetailController
-from controllers.response_builder import ResponseBuilder
-from mocks.friend_requests_successful_mock import friends_requests_successful_mock, friends_requests_raw_mock
-from mocks.errors_mock import no_data_found_mock, no_db_conn_mock
-from errors_exceptions.no_data_found_exception import NoDataFoundException
+
 from api_client.db_connection_error import DBConnectionError
+from constants import MONGODB_USER, MONGODB_PASSWD
+from controllers.be_friend_detail_controller import BeFriendDetailController
+from controllers.db_controller import MongoController
 from controllers.error_handler import ErrorHandler
+from mocks.errors_mock import no_db_conn_mock
+from mocks.friend_requests_successful_mock import friends_requests_successful_mock, friends_requests_raw_mock, \
+    friends_requests_creation_successful_mock
+from models.friend_request import FriendRequestModel
 
 
 class TestFriendRequestApi(unittest.TestCase):
@@ -51,3 +53,21 @@ class TestFriendRequestApi(unittest.TestCase):
         service._create_get_friends_requests_response = \
             mock.MagicMock(return_value=friends_requests_successful_mock)
         self.assertEqual(service.get_friends_requests_sent_by_user_id(user_id), friends_requests_successful_mock)
+
+    @mock.patch('models.friend_request.MongoController')
+    def test_create_friend_request_successful(self, db_controller_name_mock):
+        db_controller_mock = db_controller_name_mock.return_value
+        side_effects = [mock.Mock(), MongoController.get_mongodb_instance(MONGODB_USER, MONGODB_PASSWD),
+                        MongoController.get_mongodb_instance(MONGODB_USER, MONGODB_PASSWD),
+                        MongoController.get_mongodb_instance(MONGODB_USER, MONGODB_PASSWD),
+                        MongoController.get_mongodb_instance(MONGODB_USER, MONGODB_PASSWD),
+                        MongoController.get_mongodb_instance(MONGODB_USER, MONGODB_PASSWD)]
+        db_controller_mock.get_mongodb_instance = mock.Mock(side_effect=side_effects)
+        FriendRequestModel.friend_request_exists = mock.MagicMock(return_value= False)
+
+        request = FriendRequestModel.create_friend_request(1, 5, 'hello', 'picId')
+
+        self.assertEqual(request["user_id_sender"], friends_requests_creation_successful_mock["user_id_sender"])
+        self.assertEqual(request["user_id_rcv"], friends_requests_creation_successful_mock["user_id_rcv"])
+        self.assertEqual(request["message"], friends_requests_creation_successful_mock["message"])
+        self.assertEqual(request["picture"], friends_requests_creation_successful_mock["picture"])
