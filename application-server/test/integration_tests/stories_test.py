31526c14fcb8af5fd242427a7794f4884dee6607
Nicolas Dominguez <nicolas.dominguez@avature.net>
2018-07-01 19:14:18 -0300

    Test stories pudate controller

--- application-server/test/integration_tests/stories_test.py
+++ application-server/test/integration_tests/stories_test.py
@@ -21,6 +21,13 @@ class TestStoriesApi(unittest.TestCase):
         response = self.app.get(url, headers=headers)
         return response
 
+    def __make_put_request(self, story_id, request):
+        url = 'api/v1/stories/' + story_id
+        headers = {'Content-type': 'application/json',
+                   'Accept': 'text/plain'}
+        response = self.app.put(url, headers=headers, data=json.dumps(request))
+        return response
+
     @staticmethod
     def __get_response_data(response):
         return json.loads(response.get_data().decode(sys.getdefaultencoding()))
@@ -35,3 +42,29 @@ class TestStoriesApi(unittest.TestCase):
         response_data = self.__get_response_data(response)
         self.assertTrue(isinstance(response_data, list))
         self.assertTrue(all(story["story_type"] == "normal" for story in response_data))
+
+    @patch('controllers.storie_detail_controller.UserActivityModel.log_storie_activity')
+    @patch('controllers.storie_detail_controller.StorieModel')
+    @patch('controllers.storie_detail_controller.validate_sender')
+    @patch('auth_service.is_authenticated')
+    def test_successful_put_stories_should_return_status_200(self, auth_mock, validate_mock, model_mock, log_mock):
+        auth_mock.return_value = True
+        model_mock.get_stories.return_value = {'user_id': 'mock'}
+        model_mock.update_storie.return_value = {'user_id': 'mock', '_id': 'mock'}
+        user_id = '10'
+        storie_id = '1'
+        request = {
+            'user_id': user_id,
+            '_id': storie_id,
+            '_rev': '',
+            'title': '',
+            'location': '',
+            'visibility': '',
+            'multimedia': '',
+            'story_type': 'normal'
+        }
+
+        response = self.__make_put_request(storie_id, request)
+
+        self.assertEqual(200, response.status_code)
+        model_mock.update_storie.assert_called_with(storie_id, request)
